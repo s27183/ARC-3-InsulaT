1bc66a48270fbcf638c1db364190350677b9d445
s27183 <s27183@panalogy-lab.com>
2025-10-08 11:40:37 +1100

    Simplify import path in `__init__.py` by removing relative reference to `dt_agent`.

--- agents/__init__.py
+++ agents/__init__.py
@@ -12,7 +12,7 @@ from .templates.llm_agents import LLM, FastLLM, GuidedLLM, ReasoningLLM
 from .templates.random_agent import Random
 from .templates.reasoning_agent import ReasoningAgent
 from .templates.smolagents import SmolCodingAgent, SmolVisionAgent
-from ..dt_agent import *
+from dt_agent import *
 
 load_dotenv()
 
